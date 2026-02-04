8584c603a33af9ce0e61af41533eada1d06e6f77
sumit <1920sumitpandey@gmail.com>
2026-02-04 21:23:31 +0530

    LargestRectangleInHistogram

---
+++ src/main/java/StackAndQueues/LargestRectangleInHistogram.java
@@ -0,0 +1,28 @@
+package StackAndQueues;
+
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        System.out.println(findLargestRectangleInHistogram(heights));
+    }
+
+    public static int findLargestRectangleInHistogram(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n ? 0 : heights[i]);
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
+
